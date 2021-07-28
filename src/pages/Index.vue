4c9776e585f970bf186326ab74f00c8741d6c158
Matheus Vitor <m4theusdev@gmail.com>
2021-07-28 18:24:58 -0300

    :sparkles: - Feature: Background

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        <p>My personal portfolio</p>
-    </div>
+    <main></main>
 </template>
+<style scoped>
+    main{
+        background: url('../assets/images/wallpapper.png');
+        background-size: 100%;
+        height: 100vh;
+    }
+</style>
