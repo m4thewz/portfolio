d9749a33c4a5e3553d1583b9666b6071aae31ca2
Matheus Vitor <m4theusdev@gmail.com>
2021-07-28 12:38:50 -0300

    Add fonts

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <router-view/>
+  <div id="app">
+      <router-view/>
+  </div>
 </template>
+
+<style>
+  #app {
+    font-family: Rainy Hearts;
+  }
+  @font-face {
+      font-family: "Rainy Hearts";
+      src: url('./assets/fonts/rainyhearts.ttf');
+  }
+  @font-face {
+      font-family: "VCR OSD";
+      src: url('./assets/fonts/VCR_OSD.ttf');
+  }
+</style>
