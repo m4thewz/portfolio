3f6f5ced0b8f36bd9deee75099a63b76c827ac49
Matheus Vitor <m4theusdev@gmail.com>
2021-07-28 19:46:53 -0300

    :sparkles: - Main page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,33 @@
 <template>
-    <main></main>
+    <main>
+        <img src="../assets/images/avatar.png" alt="avatar">
+        
+        <h1>Matheus Vitor</h1>
+        <p>Uma pessoa que gosta de contribuir</p>
+    </main>
 </template>
 <style scoped>
     main{
+        padding-top: 100px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        color: white;
         background: url('../assets/images/wallpapper.png');
         background-size: 100%;
         height: 100vh;
+    }
+    img{
+        width: 250px;
+        border-radius: 100%;
+    }
+    h1{
+        font-size: 36px;
+        padding-top: 5px;
+    }
+    p{
+        font-size: 22px;
+        padding-top: 15px;
     }
 </style>
