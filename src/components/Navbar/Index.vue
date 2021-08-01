2ca6730fc2300eb35b98b149992063566899d707
Matheus Vitor <m4theusdev@gmail.com>
2021-08-01 20:39:26 -0300

    :recycle: - Fixed Navbar

--- src/components/Navbar/Index.vue
+++ src/components/Navbar/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="flex justify-between items-center py-3 px-4">
+    <nav class="fixed flex justify-between items-center py-3 px-4 w-screen">
         <h1 class="text-4xl font-medium">M4theus.me</h1>
         <ul class="flex space-x-3 text-xl">
             <NavItem href="/" isActive>Home</NavItem>
