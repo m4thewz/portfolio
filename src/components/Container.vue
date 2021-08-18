8549175605ad251cf2fd7fbfcf952ef8c3540126
Matheus Vitor <m4theusdev@gmail.com>
2021-08-17 22:48:10 -0300

    :sparkles: Finish container

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,15 +1,18 @@
 <template>
-    <main class="flex items-center justify-center h-screen">
-        <img class="rounded-full w-1/4" src="../assets/images/avatar.png" alt="avatar">
+    <main class="flex flex-col items-center justify-center h-screen w-screen">
+        <img class="rounded-full w-1/5" src="../assets/images/avatar.png" alt="avatar">
         
-        <div class="ml-16">
-            <h1 class="text-4xl font-medium">Matheus Vitor</h1>
-            <p>Uma pessoa que gosta de contribuir</p>
+        <div class="flex flex-col justify-between items-center text-center mt-4">
+            <div>
+                <h1 class="text-4xl font-medium">Matheus Vitor</h1>
+                <p class=" mt-0.5">Uma pessoa que gosta de contribuir</p>
+            </div>
+            <button class="btn btn-blue rounded-xl font-josefin leading-6">SOBRE</button>
         </div>
     </main>
 </template>
-<style scoped>
-    main{
+<style>
+    body{
         background: url('../assets/images/wallpapper.png');
         background-size: 100%;
         background-repeat: no-repeat;
--- tailwind.js
+++ tailwind.js
@@ -3,6 +3,12 @@ module.exports = {
     fontFamily: {
      'poppins': ['"Poppins"', 'sans-serif'],
      'josefin': ['"Josefin Sans"', 'sans-serif']
+    },
+    extend: {
+      spacing: {
+        '35': '35px',
+        '45': '45px'
+      }
     }
   },
 }
