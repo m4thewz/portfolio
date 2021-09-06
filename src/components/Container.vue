c5bed74b22f6ad6afc873f889547325a7024507e
Matheus Vitor <m4theusdev@gmail.com>
2021-09-06 09:03:14 -0300

    :sparkles: Added texts in container

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -2,8 +2,8 @@
     <main class="flex items-center justify-between h-screen w-screen py-5 px-14">
         <div class="flex flex-col items-center">
             <div>
-                <h1 class="text-4xl font-medium">Matheus Vitor</h1>
-                <p class=" mt-0.5">Uma pessoa que gosta de contribuir</p>
+                <span class="font-josefin uppercase">Uma pessoa que gosta de contribuir</span>
+                <h2 class="text-6xl font-bold uppercase">Web Developer</h2>
             </div>
         </div>
         <img class="rounded-full w-1/5" src="../assets/images/avatar.png" alt="avatar">
