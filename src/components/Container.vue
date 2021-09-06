077df0a869a74b13b9a517f937ba1c8dfb85f019
Matheus Vitor <m4theusdev@gmail.com>
2021-09-06 07:54:43 -0300

    :art: Aligninged container elements

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,14 +1,12 @@
 <template>
-    <main class="flex flex-col items-center justify-center h-screen w-screen">
-        <img class="rounded-full w-1/5" src="../assets/images/avatar.png" alt="avatar">
-        
-        <div class="flex flex-col justify-between items-center text-center mt-4">
+    <main class="flex items-center justify-between h-screen w-screen py-5 px-14">
+        <div class="flex flex-col items-center">
             <div>
                 <h1 class="text-4xl font-medium">Matheus Vitor</h1>
                 <p class=" mt-0.5">Uma pessoa que gosta de contribuir</p>
             </div>
-            <button class="btn btn-blue rounded-xl font-josefin leading-6">SOBRE</button>
         </div>
+        <img class="rounded-full w-1/5" src="../assets/images/avatar.png" alt="avatar">
     </main>
 </template>
 
