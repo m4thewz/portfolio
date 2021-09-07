b74be9ade1c1e9a762201b66868f4e1abb3611e7
Matheus Vitor <m4theusdev@gmail.com>
2021-09-07 18:40:02 -0300

    :sparkles: Added material icons

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,8 @@
 <template>
-    <Container/>
+    <div class="flex flex-col items-center jusify-center relative">
+        <Container/>
+        <span class="absolute bottom-0 animate-bounce text-8xl material-icons">expand_more</span>
+    </div>
 </template>
 <script>
   import Container from '@/components/Container/'
