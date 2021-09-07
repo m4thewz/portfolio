25dde0e2b2b714f44342017e17035fd450715e00
Matheus Vitor <m4theusdev@gmail.com>
2021-09-07 18:45:19 -0300

    :art: Added index in navbar

--- src/components/Navbar/Index.vue
+++ src/components/Navbar/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="fixed flex justify-between items-center py-3 px-4 w-screen">
+    <nav class="z-10 fixed flex justify-between items-center py-3 px-4 w-screen">
         <h1 class="text-4xl font-bold">Matheus Vitor</h1>
         <ul class="flex space-x-3 text-xl font-josefin">
             <NavItem href="/">Home</NavItem>
