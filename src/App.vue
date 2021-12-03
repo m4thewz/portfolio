bd206c0c2e3efcd883f6c69a69fe172b237098cb
Matheus Vitor <m4theusdev@gmail.com>
2021-12-03 03:20:21 -0300

    :sparkles: Added navbar in all pages

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+
 export default {
-  components: Navbar,
+  components: { Navbar },
 };
 </script>
