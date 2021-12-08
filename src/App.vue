75db1c127b46090517fd46ff8605ca1ea3426fd5
Matheus Vitor <m4theusdev@gmail.com>
2021-12-08 09:58:49 -0300

    :art: Stylezed scrollbar

--- src/App.vue
+++ src/App.vue
@@ -28,4 +28,21 @@ export default {
   font-family: "Poppins", sans-serif;
   background-color: #f8f9fa;
 }
+::-webkit-scrollbar {
+  width: 6px;
+  height: 6px;
+}
+::-webkit-scrollbar-thumb {
+  background: #008dff;
+  border: 10px none #ffffff;
+  border-radius: 30px;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #b3afb3;
+}
+::-webkit-scrollbar-track {
+  background: #f8f9fa;
+  border-radius: 0px;
+  box-shadow: inset 0px 0px 0px 0px #f0f0f0;
+}
 </style>
